c5e8d41f708209beca3b23b3f5d3036e4adeca85
Kocsis Gergely <kocsisger@gmail.com>
2026-02-17 11:06:41 +0100

    15/16 buttons

--- app/src/main/java/hu/unideb/inf/calculator_t10/MainActivity.java
+++ app/src/main/java/hu/unideb/inf/calculator_t10/MainActivity.java
@@ -2,6 +2,8 @@ package hu.unideb.inf.calculator_t10;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView resultTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +25,18 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultTextView = findViewById(R.id.resultTextView);
     }
 
     public void handleSimpleButtonPressed(View view) {
+        Button button = (Button)view;
+        resultTextView.append(button.getText().toString());
     }
 
 
     public void handleCEButtonPressed(View view) {
+        resultTextView.setText("0");
     }
 
     public void handleEQButtonPressed(View view) {
